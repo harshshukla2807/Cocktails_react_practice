c882b7cbaadfa8c1a0629d108bff29d31b01dfe2
Harsh Shukla <harshshukla2807@gmail.com>
2023-08-15 03:38:42 +0530

    making fetch call for cocktail response

--- src/context.js
+++ src/context.js
@@ -1,15 +1,39 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
-// make sure use
-export const useGlobalContext = () => {
-  return useContext(AppContext)
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
+  
+const fetchDrinks=async()=>{
+  setLoading(true)
+  
+  try {
+    const response= await fetch(`${url}${searchTerm}`)
+    const data= await response.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+  
 }
 
-export { AppContext, AppProvider }
+useEffect(()=>{
+  fetchDrinks()
+},[searchTerm])
+  return (
+    <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
+    <div className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sed nesciunt ex quibusdam, maxime alias fugit illum eligendi magnam, eius velit sint temporibus voluptatem! Vitae deleniti quo rerum esse quae?</p>
     </div>
   )
 }
